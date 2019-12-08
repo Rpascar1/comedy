fcc8a18ef928974b64a8b32d02d0b0f4db8fdc8d
Rich <rpavesi84@gmail.com>
2019-12-08 16:45:42 -0500

    added notes

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -3,11 +3,13 @@ before_action :authenticate_user!, except: [:index]
 
   def index
     @clubs = Club.all
-    
+    #SET THE INSTANCE OF ALL CLUBS
   end
 
   def show
+    #SHOW ACTION SEPARATE FROM THE SHOW CLASS
     @club = Club.find_by(params[:id])
+    #SET THE INSTNACE OF CLUB EQUAL TO THE CLUB LISTED IN THE PARAMS BY ID
   end
 
 end

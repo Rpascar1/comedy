5f3f77c5c073a3a5606f7a76ed66da53fdeb0335
Rich <rpavesi84@gmail.com>
2019-12-08 16:45:52 -0500

    added notes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
 
   def home
-
+    #CREATED OUTSIDE NORMAL RESTFUL CONVENTION FOR HOMEPAGE SEPARATING
+    #AUTH ACTIONS
+    #EXITS IN ROUTE ONLY NEEDED TO RENDER VIEW NO LOGIC 
   end
 
 end
